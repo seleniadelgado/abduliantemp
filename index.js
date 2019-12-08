136c89702f860bebd17caf7ede422775ec93379b
Shoji <stakashima21@gmail.com>
2019-12-08 15:02:17 -0800

    feat(index.js): Implements usage of ejs templating

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var PORT = process.env.PORT || 5000;
+var path = require('path');
 var express = require('express');
-var app = express();
+var expressLayouts = require('express-ejs-layouts');
 
+// Routes
+indexRouter = require('./routes/home');
+
+var app = express();
 var http = require('http');
 var server = http.Server(app);
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static('./'));
 app.use(express.static('src'));
+app.use(expressLayouts);
+app.use(express.json());
+
+app.use('/', indexRouter);
 
 server.listen(PORT, function() {
 	console.log('Server is running');
