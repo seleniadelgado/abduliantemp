c75d2a1f8d9b09e6a174775a1241b347b752edf1
Shoji <stakashima21@gmail.com>
2020-01-03 17:38:19 -0800

    build(*): Update scripts and dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true,
+        "jquery": true
+    },
+    "globals": {
+        "Waypoint": true,
+        "google": true,
+        "gmap": true
+    },
+    "rules": {
+        "vars-on-top": "off",
+        "no-var": "off",
+        "prefer-arrow-callback": "off",
+        "func-names": "off",
+        "prefer-destructuring": "off"
+    }
+};
