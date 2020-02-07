b884c71b702c0bae870ca72998e194870fa0b1e6
Shoji <stakashima21@gmail.com>
2020-02-07 08:55:59 -0800

    refactor(views): update views with new data structure

--- routes/settlements.js
+++ routes/settlements.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    const data = [
-        {'total': '$52,200'},
-        {'total': '$1 Million'},
-        {'total': '$11,432'},
-        {'total': '$1 Million'},
-        {'total': '$5 Million'},
-    ];
+    const data = {
+        'settlements': [
+            {'total': '$52,200'},
+            {'total': '$1 Million'},
+            {'total': '$11,432'},
+            {'total': '$1 Million'},
+            {'total': '$5 Million'},
+        ],
+    };
+
     res.render('settlements', {data: data});
 })
 
